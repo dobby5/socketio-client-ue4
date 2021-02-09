283bd18e73d0b7f424470939421812ae34acfd37
Peter Gusev <peter@remap.ucla.edu>
2021-02-09 14:06:25 -0800

    [ptn] Build.cs setup for OpenSSL

--- Source/SocketIOLib/SocketIOLib.Build.cs
+++ Source/SocketIOLib/SocketIOLib.Build.cs
@@ -57,6 +57,30 @@ namespace UnrealBuildTool.Rules
 	            {
 	            }
 	            );
-	    }
+
+
+			if (Target.Platform == UnrealTargetPlatform.Win64)
+			{ // setup TLS support 
+
+				PublicDefinitions.Add("SIO_TLS");
+
+				PrivateIncludePaths.Add(Path.Combine(ThirdPartyPath, "openssl/x64/include"));
+				PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "openssl/x64/lib/libcrypto.lib"));
+				PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "openssl/x64/lib/libssl.lib"));
+				RuntimeDependencies.Add(Path.Combine(ThirdPartyPath, "openssl/x64/bin/libcrypto-1_1-x64.dll"));
+				RuntimeDependencies.Add(Path.Combine(ThirdPartyPath, "openssl/x64/bin/libssl-1_1-x64.dll"));
+
+				RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Binaries/Win64/libcrypto-1_1-x64.dll"),
+					Path.Combine(ThirdPartyPath, "openssl/x64/bin/libcrypto-1_1-x64.dll"));
+				RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Binaries/Win64/libssl-1_1-x64.dll"),
+					Path.Combine(ThirdPartyPath, "openssl/x64/bin//libssl-1_1-x64.dll"));
+
+				RuntimeDependencies.Add("$(TargetOutputDir)/libcrypto-1_1-x64.dll",
+					Path.Combine(ThirdPartyPath, "openssl/x64/bin/libcrypto-1_1-x64.dll"));
+				RuntimeDependencies.Add("$(TargetOutputDir)/libssl-1_1-x64.dll", 
+					Path.Combine(ThirdPartyPath, "openssl/x64/bin/libssl-1_1-x64.dll"));
+
+			}
+		}
 	}
 }
